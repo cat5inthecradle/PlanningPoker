ce1e07a181955b8f15004229fd1cccc26ef80b72
Darin Webb <dwebb@sourceallies.com>
2016-04-01 13:48:04 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Planning Poker</title>
+    <meta name="description" content="Agile Poker For Face-To-Face Planning">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+
+    <div class="container">
+
+        <ul class="nav nav-pills">
+            <li class="active"><a href="#unk" data-toggle="tab">?</a></li>
+            <li><a href="#1" data-toggle="tab">1</a></li>
+            <li><a href="#3" data-toggle="tab">3</a></li>
+            <li><a href="#5" data-toggle="tab">5</a></li>
+            <li><a href="#8" data-toggle="tab">8</a></li>
+            <li><a href="#13" data-toggle="tab">13</a></li>
+        </ul>
+
+        <section class="tab-content">
+            <article class="tab-pane active" id="unk">
+                <h3>?</h3>
+            </article>
+            <article class="tab-pane" id="1">
+                <h3>1</h3>
+            </article>
+            <article class="tab-pane" id="3">
+                <h3>3</h3>
+            </article>
+            <article class="tab-pane" id="5">
+                <h3>5</h3>
+            </article>
+            <article class="tab-pane" id="8">
+                <h3>8</h3>
+            </article>
+            <article class="tab-pane" id="13">
+                <h3>13</h3>
+            </article>
+        </section>
+
+    </div>
+
+
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
+
+</body>
+</html>
